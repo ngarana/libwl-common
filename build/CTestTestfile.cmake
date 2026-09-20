7ee9ca4ad9209ca8c9889a58cc5727637a5db824
namatanda <stanford.githimu@court.go.ke>
2026-09-20 14:14:19 +0300

    feat: merged IconResolver + icon tests

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -6,3 +6,5 @@
 # testing this directory and lists subdirectories to be tested as well.
 add_test("blur_test" "/home/arch/Coding/libwl-common/build/blur_test")
 set_tests_properties("blur_test" PROPERTIES  _BACKTRACE_TRIPLES "/home/arch/Coding/libwl-common/CMakeLists.txt;26;add_test;/home/arch/Coding/libwl-common/CMakeLists.txt;0;")
+add_test("icon_test" "/home/arch/Coding/libwl-common/build/icon_test")
+set_tests_properties("icon_test" PROPERTIES  _BACKTRACE_TRIPLES "/home/arch/Coding/libwl-common/CMakeLists.txt;36;add_test;/home/arch/Coding/libwl-common/CMakeLists.txt;0;")
